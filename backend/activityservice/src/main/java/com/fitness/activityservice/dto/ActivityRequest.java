bf2ca6913b9f1bee80269c87a3047eadbef6a441
Praneswar G <praneswarganesan@gmail.com>
2025-04-12 21:29:13 +0530

    acitvityservice completed with its endpoints

--- backend/activityservice/src/main/java/com/fitness/activityservice/dto/ActivityRequest.java
+++ backend/activityservice/src/main/java/com/fitness/activityservice/dto/ActivityRequest.java
@@ -1,19 +1,18 @@
 package com.fitness.activityservice.dto;
+
 import com.fitness.activityservice.model.ActivityType;
 import lombok.Data;
 
-
 import java.time.LocalDateTime;
 import java.util.Map;
+
 @Data
-public class ActivityResponse {
-    private String id;
-    private String userId;
+public class ActivityRequest {
+    private  String userId;
     private ActivityType type;
     private Integer duration;
     private Integer caloriesBurned;
-    private LocalDateTime startTime;
     private Map<String, Object> additionalMetrics;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+    private LocalDateTime startTime;
+    private LocalDateTime updatedTime;
 }
