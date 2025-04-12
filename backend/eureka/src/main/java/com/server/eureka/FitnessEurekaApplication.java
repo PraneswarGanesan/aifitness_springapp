d773c13c7065c4eb92a39f73aec7d49379efdbe5
Praneswar G <praneswarganesan@gmail.com>
2025-04-13 04:26:35 +0530

    eureka server creation and discrover client on user-service

---
+++ backend/eureka/src/main/java/com/server/eureka/FitnessEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.server.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class FitnessEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessEurekaApplication.class, args);
+	}
+
+}
